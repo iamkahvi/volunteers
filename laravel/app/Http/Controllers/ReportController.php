fbfe9f3b873489c932cea249f40e39d0552e73b6
Kahvi <iamkahvi@gmail.com>
2018-06-28 14:58:35 -0400

    Report Controller bug fix

--- laravel/app/Http/Controllers/ReportController.php
+++ laravel/app/Http/Controllers/ReportController.php
@@ -12,6 +12,12 @@ use App\Models\Event;
 use App\Models\Slot;
 use Carbon\Carbon;
 
+if (version_compare(PHP_VERSION, '7.2.0', '>=')) {
+    // Ignores notices and reports all other kinds... and warnings
+    error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+    // error_reporting(E_ALL ^ E_WARNING); // Maybe this is enough
+}
+
 class ReportController extends Controller
 {
     // Require admin authentication
