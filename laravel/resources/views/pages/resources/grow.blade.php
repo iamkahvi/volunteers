e0b4d0e2c3d28825e64b489d892fc7bb62aebb7c
Loving Spoonful <41695559+lovingspoonful@users.noreply.github.com>
2018-08-20 10:32:05 -0400

    Updated content

--- laravel/resources/views/pages/resources/grow.blade.php
+++ laravel/resources/views/pages/resources/grow.blade.php
@@ -3,23 +3,67 @@
 @section('content')
 <div class="col-sm-2"></div>
 <div class="col-sm-8">
-    <h2>GROW Project</h2>
-    <p>Loving Spoonful&rsquo;s GROW Project teaches elementary school students about community and collaboration, local food systems and stewardship, and gardening and cooking through classroom and garden workshops.</p>
-    <p>GROW Project Volunteers will choose to:</p>
-    <p>1) Assist with in-class and in-garden workshops that occur during the school year (in April, May &amp; June and Oct &amp; Nov) at our GROW schools <strong>and/or</strong></p>
-    <p>2) Look after one GROW school garden during the summer (July &amp; August)</p>
+    <h1>Volunteer Position: GROW Project Workshop Assistant</h1>
+    <h3>Program Description</h3>
+    <p>Loving Spoonful's school-garden based GROW Project teaches over 600 students at 17 Kingston and area schools about good food, community, and collaboration. In each school, students in grades 4 to 8 participate in seven curriculum-connected classroom workshops that complement their garden time, teaching students about food systems, social justice and food security, food and culture, and healthy gardens and cooking. Visits from local farmers and nature-based field trips further assist students to make connections between their food, their health, their community, and the Earth.</p>
+    <p>The GROW Project is one of several programs that directly support Loving Spoonful’s goals of enhancing access to healthy food for all; engaging diverse sectors of the community; and delivering innovative and impactful programming.
+    <h3>Position Purpose &ndash; Your impact</h3>
+    <p>GROW Project Workshop Assistants help out in hands-on workshops where students design, plant, care for and harvest vegetables in their school gardens while learning values of stewardship and responsibility.</p>
+    <h3>Qualifications/Resources &ndash; What you&rsquo;ll need</h3>
     <ul>
-    <li><em>In-school and summer volunteer shifts are approximately 2 hours in length.</em></li>
+    <li>A Criminal Record Check with ‘no criminal record’ is required. Cost is reimbursed; just provide your receipt.</li>
+    <li>Ability to lead small groups (e.g. chopping vegetables, sowing seeds, looking at vermi-composting worms); a staff person from Loving Spoonful will always be present.</li>
+    <li>Knowledge of basic gardening techniques such as “planting”, “weeding”, “thinning” and “harvesting” would be helpful for spring workshops, but is not required.</li>
+    <li>Knowledge of basic food preparation techniques (chopping, mixing, using different ingredients) would be helpful but is not required.</li>
+    <li>Light physical labour is required for some of the workshops.</li>
     </ul>
-    <p>During the in-school workshops, you will assist with workshop activities as required, including set up and clean up, small group work, and one-on-one interactions (e.g., ensuring knife safety when cooking, helping with seedling transplanting, planting gardens, etc.). When appropriate, you will talk to students about relevant topics to generate discussion around the activity.</p>
-    <p>If volunteering in the summer program, you will be responsible for ensuring the garden is well watered and weeded a minimum of once per week. You will also help with thinning of the crops as needed, and will need to make sure cuttings and weeds are disposed of. This will include harvesting any ripe produce and ensuring it is delivered to Loving Spoonful as needed.</p>
-    <p>Students are informed of the weekly garden sessions during the summer to encourage them to come help out. It is therefore necessary to have a consistent day and time for your garden visit each week. You may choose from the following shifts:</p>
+    <h3>Key Duties and Responsibilities</h3>
+    <p>Workshop Assistants help in the classroom and/or garden with students from grades 4 to 8. Their role could include:</p>
     <ul>
-    <li>Any day Monday &ndash; Friday</li>
-    <li>Morning (8:30 am &ndash; 10:45 am), midday (11 am &ndash; 1 pm), afternoon (2 pm &ndash; 3:30 pm)</li>
+    <li>Helping students to prepare food – salsa, burritos, smoothies, or home-grown salad (fall & spring)</li>
+    <li>Helping students discuss barriers & solutions to healthy eating (fall)</li>
+    <li>Assisting students as they design and plant their garden (spring)</li>
+    <li>Assisting students as they transplant tomato seedlings and explore vermi-composting (spring)</li>
+    <li>In each workshop: Helping with set up, clean-up and interacting with students</li>
     </ul>
-    <p>Volunteers must be comfortable interacting with students and generating garden-related conversations with them.</p>
-    <p>Volunteers must be able to transport garden equipment and harvested produce to/from garden as required. Loving Spoonful will provide the garden tools and harvest bags.</p>
-    <p>A Criminal Record Check (CPIC) is required to volunteer with the GROW Program (in-school and summer positions).</p>
+    <p>Specific volunteer position details will be available for each workshop opportunity so volunteers can choose the positions that are right for them.</p>
+    <p><u>Other responsibilities</u></p>
+    <ul>
+    <li>Sign up for shifts on the online Volunteer Robot</li>
+    <li>Cancel shifts on the online Volunteer Robot at least 3 days in advance if going to be absent, or directly inform the Loving Spoonful staff as soon as possible if last minute changes occur.</li>
+    </ul>
+    <h3>Time Commitments and Locations</h3>
+    <p>Over the school year, the GROW Project delivers 7 workshops to each of 17 different schools in the City of Kingston. GROW Workshop Assistants are needed for 5 of these workshops (85 workshops in total) which take place in: late Sept/early Oct; November; April; May; and June.</p>
+    <p>All scheduled GROW Workshops are posted on Loving Spoonful’s online Volunteer Robot, and each GROW Workshop Assistant will be provided an online account to sign up to volunteer for any workshop. Workshop shifts can occur any week day, Monday to Friday, during school hours, and are approximately 2-3 hours long.</p>
+    <h3>Support &ndash; What we&rsquo;ll provide</h3>
+    <ul>
+    <li>Hands-on, on-site training by GROW Project Staff</li>
+    <li>An account with Loving Spoonful’s Volunteer Robot to view and sign up for shifts</li>
+    <li>You are always free to contact the GROW Project Coordinator and/or Volunteer Coordinator to discuss any concerns or provide feedback.</li>
+    </ul>
+    <h3>Benefits &ndash; What you&rsquo;ll gain</h3>
+    <ul>
+    <li>Great experience teaching and working with youth</li>
+    <li>The chance to work in – and learn more about – vegetable gardens</li>
+    <li>Reference letter supplied for successful volunteer experience</li>
+    <li>Cost of CPIC will be reimbursed with submission of a receipt</li>
+    </ul>
+    <h3>Measures of Success</h3>
+    <p>GROW Workshop Assistants will observe the positive effects of their presence when they observe:</p>
+    <ul>
+    <li>Students excited about healthy food and keen to try new things</li>
+    <li>Increased awareness of food systems, food security and the importance of food in culture</li>
+    <li>Building a connection to the environment and working together to support the local community</li>
+    <li>The development of life skills such as gardening, healthy food preparation, and critical thinking</li>
+    </ul>
+    <h3>Responsible to:</h3>
+    <p>Anne Munier, GROW Project Coordinator <a href="mailto:AnneM@lovingspoonful.org">AnneM@lovingspoonful.org</a></p>
+    <h3>Next Steps</h3>
+    <ul>
+    <li>Review and sign off on the Loving Spoonful Volunteer Rights &amp; Code of Conduct</li>
+    <li>Receive an account with Volunteer Robot (provided)</li>
+    <li>Attend a Loving Spoonful Orientation</li>
+    <li>Attend a GROW Project training (This may occur at the same time as the Loving Spoonful Orientation, or be conducted prior to your first workshop.)</li>
+    </ul>
 </div>
 @endsection
