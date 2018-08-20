66be90b2a8e1e774c34256d3a33512c478be5bce
Loving Spoonful <41695559+lovingspoonful@users.noreply.github.com>
2018-08-20 14:45:16 -0400

    Content update

--- laravel/resources/views/pages/resources/kitchens.blade.php
+++ laravel/resources/views/pages/resources/kitchens.blade.php
@@ -3,18 +3,60 @@
 @section('content')
 <div class="col-sm-2"></div>
 <div class="col-sm-8">
-    <h2>Community Kitchens</h2>
-    <p>Community Kitchen Volunteer Assistant Instructors help the Community Kitchen Instructor in leading healthy cooking classes with groups of people of all ages at various locations in the City (e.g., Kingston Community Health Centres, Princess Street United Church, etc.). Classes vary from 2-4 hours in length. Shifts can be during the day, evening, or weekends. Volunteers can commit to either one-off or regular volunteer positions (such as a 4 &ndash; 6 week cooking series). Cooking programs run between September and June each year. Note that Community Kitchen positions are limited, so please sign up for another volunteer role with Loving Spoonful in case we are unable to fit you into the kitchen.</p>
-    <p>A training meeting will be set up at the start of each cooking program as required. The Community Kitchen Instructor will review details about the cooking participants and volunteer position, as well as the recipes and activities that will be used during the workshops. This meeting will either take place before the program begins or directly before the first shift at the kitchen.</p>
-    <p>The Community Kitchen Instructor will provide support to the volunteer. Supervision will take place to make sure recipes are being followed (within reason) and people are having fun.</p>
-    <p>Community Kitchen volunteers will:</p>
+    <h1>Community Kitchens Workshop Facilitator</h1>
+    <h3>Program Description</h3>
+    <p>Loving Spoonful’s Community Kitchens are designed to get people together around good food. We cook with people of all ages, backgrounds, and abilities – children, youth, adults, families, newcomers, Indigenous community members, people with physical and intellectual differences, expert home cooks, and beginners. We get together to make healthy, affordable, and simple recipes in kitchens throughout Kingston. Hands-on learning and discussions of food topics helps build knowledge, skills, and confidence, encouraging participants to make healthy and informed food choices in the kitchen and beyond. In addition to food literacy development, our community kitchens are places where people socialize, building community connections and friendships around food. They also provide good food access as surplus food, healthy recipes, spices, and cookware are sent home to assist with cooking after programs are complete.</p>
+    <p>Community Kitchens is one of several programs that directly support Loving Spoonful’s goals of enhancing access to healthy food for all; engaging diverse sectors of the community; and delivering innovative and impactful programming.
+    <h3>Position Purpose &ndash; Your impact</h3>
+    <p>Creating a space where participants can build food literacy, skills, confidence, and community around good food, Community Kitchen Workshop Facilitators are critical in helping to ensure that the cooking classes run smoothly and are positive experiences for all participants. They provide extra hands, eyes and words of support and encouragement. Workshop Facilitators will observe the positive effects of their presence week-to-week as participants’ skills improve, confidence builds, and friendships begin.</p>
+    <h3>Qualifications/Resources &ndash; What you&rsquo;ll need</h3>
     <ul>
-    <li>Assist in leading a small group of participants through the workshop recipes;</li>
-    <li>Engage with participants in a fun and encouraging way;</li>
-    <li>Help with set up, clean up, and workshop activities required; and</li>
-    <li>Read the recipe in advance, and give feedback about the class as required.</li>
+    <li>A non-judgemental and respectful approach is always required when working with cooking participants, ensuring participants feel welcome, included, and dignified</li>   <li>Knowledge of basic gardening techniques such as “planting”, “weeding”, “thinning” and “harvesting” would be helpful for spring workshops, but is not required.</li>
+    <li>Some cooking skills/knowledge</li>
+    <li>An outgoing attitude and an ability to engage with others</li>
+    <li>Be comfortable standing for long periods of time (1.5 – 3 hrs depending on the program)</li>
+    <li>Light physical labour is required for some of the workshops.</li>
+    <li>A Criminal Record Check is required, as program participants can bring their children to the classes. Cost is reimbursed; just provide your receipt.</li>
     </ul>
-    <p>It&rsquo;s essential to allow the participants to do most of the cooking. Volunteers are there as support, and while they can show participants how to chop/measure etc. in ways that are safe and time efficient, it&rsquo;s important they don&rsquo;t take over and do the task themselves (even if they can do them &lsquo;better&rsquo;).</p>
-    <p>A non-judgemental and respectful approach is always required when working with cooking participants, ensuring participants feel welcomed, included, and dignified.</p>
+    <h3>Key Duties and Responsibilities</h3>
+    <p><u>On-site Duties</u></p>
+    <ul>
+    <li>Assist in leading a small group of participants through the workshop recipes, allowing participants to take the lead on making the meal and demonstrating specific skills/tips as required.</li>
+    <li>Engage with participants in a fun and encouraging way while ensuring kitchen safety.</li>
+    <li>Help with set up, clean up, dishes, and workshop activities required.</li>
+    </ul>
+    <p><u>Other responsibilities</u></p>
+    <ul>
+    <li>Sign up for shifts on the online Volunteer Robot</li>
+    <li>Cancel shifts on the online Volunteer Robot at least 3 days in advance if going to be absent, or directly inform Loving Spoonful staff as soon as possible if last minute changes occur.</li>
+    </ul>
+    <h3>Time Commitments and Locations</h3>
+    <ul>
+    <li>Community Kitchen classes are offered as a series or one-time workshops. Programs specify: theme/Focus, location, number of classes (from 1 to 9), frequency of classes (single/one-off, weekly or monthly), length of classes (from 2 to 4 hours), time of classes, and number of volunteer positions needed.</li>
+    <li>Volunteer opportunities vary throughout the year according to what programs are running.</li>
+    <li>Shifts can be during the day, evening, or weekends</li>
+    <li>It is preferred that a volunteer commit to a whole program as the continuity of their presence is enriching to participants. One-off/single-class programs are also offered.</li>
+    <li>All scheduled Community Kitchens Classes are posted on Loving Spoonful’s online Volunteer Robot, and each Workshop Facilitator will be provided an online account to sign up to volunteer for any class that has an unfilled volunteer shift.</li>
+    </ul>
+    <h3>Support &ndash; What we&rsquo;ll provide</h3>
+    <ul>
+    <li>A training meeting will be set up as at the start of each cooking program to review details about the cooking participants and volunteer position, as well as the recipes and activities that will be used during the workshops. This meeting will either take place before the program begins or directly before the first shift at the kitchen.</li>
+    <li>The Community Kitchen Instructor will provide on-going support. Supervision will take place to make sure recipes are being followed (within reason) and people are having fun. </li>
+    <li>Informal post-class debriefings with the Community Kitchen Instructor allows Workshop Facilitators to discuss what is going well with the program, areas for improvement, as well as voice any suggestions, comments or concerns.</li>
+    <li>An account with Loving Spoonful’s Volunteer Robot to view and sign up for shifts</li>
+    </ul>
+    <p>You are always welcome and encouraged to contact us to share your successes, discuss any concerns or provide feedback: </p>
+    <ul>
+    <li>Thea Zuiker, Community Kitchen Coordinator: 333-343-5699, <a href="mailto:Thea@lovingspoonful.org">Thea@lovingspoonful.org</a></li>
+    <li>Mary Gauthier, Volunteer & Outreach Coordinator: <a href="mailto:Mary@lovingspoonful.org">Mary@lovingspoonful.org</a></li>
+    </ul>
+    <h3>Benefits &ndash; What you&rsquo;ll gain</h3>
+    <ul>
+    <li>Social interaction with the community</li>
+    <li>Learning new healthy recipes</li>
+    <li>Teaching and cooking in the kitchen</li>
+    <li>Cost of CPIC will be reimbursed with submission of a receipt</li>
+    <li>Reference letter supplied for successful volunteer experience</li>
+    </ul>
 </div>
 @endsection
