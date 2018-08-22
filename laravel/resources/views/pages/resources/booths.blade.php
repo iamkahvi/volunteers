2a8ad13e582f2458330cd4df8dfebf189e6c3d66
Loving Spoonful <41695559+lovingspoonful@users.noreply.github.com>
2018-08-22 15:59:04 -0400

    Content update

--- laravel/resources/views/pages/resources/booths.blade.php
+++ laravel/resources/views/pages/resources/booths.blade.php
@@ -3,48 +3,80 @@
 @section('content')
 <div class="col-sm-2"></div>
 <div class="col-sm-8">
-    <h2>Grow A Row Market Booths</h2>
-    <p>Grow a Row Market Booth Representatives will be in charge of running the Loving Spoonful Booth at the Memorial Centre Farmers&rsquo; Market (Sundays 9am-2pm) and the Downtown Kingston Public Market (Tuesdays &amp; Thursdays 3&ndash;5 pm, Saturdays 1&ndash;5 pm). These volunteers will be responsible for collecting, weighing, and recording donated produce, as well as engaging with curious passers-by and answering questions about who we are and what we do.</p>
-    <h3>DAILY TO-DO LIST</h3>
+    <h1>GROW A Row Market Booth Ambassador</h1>
+    <h3>Program Description</h3>
+    <p>Grow A Row encourages farmers, gardeners and local workplaces to grow an extra row of produce for delivery to 40 local agencies serving some of the over 20,000 people in the Kingston Area who live below the poverty line.  In 2017, Loving Spoonful collected 30,000 lbs of fresh produce that was delivered to the community.</p>
+    <p>Grow A Row Program is one of several programs that directly support Loving Spoonful’s goals of enhancing access to healthy food for all; engaging diverse sectors of the community; and delivering innovative and impactful programming.</p>
+    <h3>Position Purpose &ndash; Your impact</h3>
+    <p>As the critical point of connection between the food donors and the service agencies, Grow a Row Market Booth Ambassadors are in charge of running the Loving Spoonful Booth at the Memorial Centre Farmers’ Market and the Downtown Kingston Public Market. These volunteers collect, weigh, and record donated produce.</p>
+    <p>Grow A Row Market Booth Representatives are also the face of Loving Spoonful in the public. As they engage with curious passers-by, they help to raise awareness about what Loving Spoonful is and what we do.</p>
+    <h3>Qualifications/Resources &ndash; What you&rsquo;ll need</h3>
+    <p>Volunteers who have the most impact and enjoy this position most:</p>
     <ul>
-    <li>Arrive 5-10 minutes before your shift starts.</li>
-    <li>Gather the materials listed in the inventory below and set up the booth. The materials for the downtown market booth will be down the stairs across the street, in a room that serves as a change room during the skating season. You will see a big sliding door on your left. Ask staff for location of the key and the combination for the lockbox. For the Memorial Centre Farmers&rsquo; Market, materials will be in the storage room inside the Memorial Centre.</li>
-    <li>Greet curious passers-by, answering their questions, offering info about Loving Spoonful, and inviting them to check out our materials, and sign up for our newsletter or to volunteer.</li>
-    <li>Greet donors, weighing their produce one type of item at a time in POUNDS (lbs), noting their name, donor type (market vendor, community garden, GROW garden, individual, other), produce, and item weight directly into the <a href="https://docs.google.com/spreadsheets/d/129SJ0PpGbllwmBbNeewVniQVuuxLOUWdEInQvr_g_Rk/edit#gid=1692344192">Grow A Row Track Sheet GoogleDoc</a> &ndash; be sure to thank them!</li>
-    <li>When your shift ends, calculate the total weight of donated produce and gather materials to return to storage.</li>
-    <li>Give the produce and total weight to the driver.</li>
-    <li>Return materials to storage.</li>
-    <li>Every other Saturday (DKPM only): use the Allen key in the tent repair kit to tighten every nut in the tent to ensure the joints don&rsquo;t loosen and bust apart.</li>
-    <li>Contact Alex (alex@lovingspoonful.org) if anyone signs up for the newsletter (send a photo of the sheet), makes a donation (arrange for pickup of funds), if anything is missing or damaged, or if you need help of any kind.</li>
+    <li>Are outgoing, or willing to talk to strangers;</li>
+    <li>Able to do some light physical labour and occasional heavy lifting; and</li>
+    <li>Are able to work as part of a team.
     </ul>
-    <h3>INVENTORY</h3>
-    <p>These items will all be in storage ready to go when you arrive.</p>
+    <h3>Key Duties and Responsibilities</h3>
+    <p>Grow A Row Market Booth Ambassadors work in teams to run the Loving Spoonful Booths at the Memorial Centre Farmers’ Market and the Downtown Kingston Public Market.</p>
+    <p><u>Assemble and/or pack-up tabling materials, table & canopy</u></p>
     <ul>
-    <li>Banner (Grow A Row @DKPM, Loving Spoonful @MCFM)</li>
-    <li>Tent &amp; repair kit (DKPM only)</li>
-    <li>Cinder blocks to weigh down tent legs (DKPM only)</li>
-    <li>Table (DKPM only)</li>
-    <li>Chairs (2 MCFM, 3 DKPM)</li>
-    <li>Veggie table cloth</li>
-    <li>Annual report</li>
-    <li>Loving Spoonful rack card</li>
-    <li>Grow A Row rack card</li>
-    <li>Recipes or other materials</li>
-    <li>Tote bags or other giveaways</li>
-    <li>Knit veggie weights</li>
-    <li>Newsletter signup</li>
-    <li>Messaging cheat sheet</li>
-    <li>Donation box</li>
-    <li>Events board (with posters taped to it)</li>
-    <li>Chalk board (for writing fun facts &amp; audience questions)</li>
-    <li>Tablet for track Grow a Row donations</li>
-    <li>Veggie ID chart</li>
-    <li>Pen, tape, string</li>
-    <li>Scale</li>
-    <li>Calculator</li>
-    <li>Coolers (2@MCFM, 3@DKPM)</li>
-    <li>Coolerpacks</li>
-    <li>Bins</li>
+    <li>Arrive 5-10 minutes before your shift starts</li>
+    <li>Gather the materials listed in the inventory and set up the booth.</li>
+    <li>Inventory list and location-specific instructions will be provided at Orientation.</li>
+    </ul>
+    <p><u>Tell people about Loving Spoonful’s mission and programming</u></p>
+    <ul>
+    <li>Greet curious passers-by, answering their questions, offering info about Loving Spoonful, and inviting them to check out our materials, and sign up for our newsletter or to volunteer</li>
+    <li>Encourage people to participate in activities offered at the table, take promotional materials & sign-up for our E-Newsletter</li>
+    </ul>
+    <p><u>Collect fresh produce and Submit reports</u></p>
+    <ul>
+    <li>Greet and thank donors</li>
+    <li>Report details of donation (donor name, donor type, each produce type, produce weight) directly into the electronic track sheet (a tablet is provided)</li>
+    </ul>
+    <p><u>End of Shift</u></p>  
+    <ul>
+    <li>Calculate the total weight of donated produce</li>
+    <li>Give the produce and total weight to the driver, and help load produce into the Driver’s vehicle</li>
+    <li>Return materials to storage.</li>
+    <li>Every other Saturday (DKPM only): use the Allen key in the tent repair kit to tighten every nut in the tent to ensure the joints don’t loosen and bust apart.</li>
+    </ul>
+    <p><u>Other responsibilities</u></p>
+    <ul>
+    <li>Take pictures of donated produce, and if able, circulate on social media, or send to supervisor</li>
+    <li>Contact Alex (alex@lovingspoonful.org) if anyone signs up for the newsletter (send a photo of the sheet), makes a donation (arrange for pickup of funds), if anything is missing or damaged, or if you need help of any kind.</li>
+    <li>Sign up for shifts on the online Volunteer Robot</li>
+    <li>Cancel shifts on the online Volunteer Robot at least 3 days in advance if going to be absent, or directly inform the Loving Spoonful staff as soon as possible if last minute changes occur.</li>
+    </ul>
+    <h3>Time Commitments and Locations</h3>
+    <p>From June to October, Market Booth Ambassadors cover shifts at 1 of 2 locations. Shifts are 2 to 2.5 hours long.</p>
+    <p>Volunteers sign up for a particular booth team, and can volunteer weekly or once or twice per month. Booth teams are:</p>
+    <ul>
+    <li>Tuesday (3:00PM – 5:00PM) at Downtown Kingston Public Market</li>
+    <li>Thursday (3:00PM – 5:00PM) at Downtown Kingston Public Market</li>
+    <li>Saturday (1:00PM – 5:00PM {1-3 pm, 3-5pm}) at Downtown Kingston Public Market</li>
+    <li>Sunday (9:00AM – 2:30PM {9-12 pm, 12-2:30}) at Memorial Centre Farmers Market</li>
+    </ul>
+    <p>Volunteers sign up for shifts through their own account (provided) on Loving Spoonful’s online Volunteer Robot.</p>
+    <p>Commitment is encouraged throughout the season; vacations are accommodated.</p>
+    <h3>Support &ndash; What we&rsquo;ll provide</h3>
+    <ul>
+    <li>A group training is provided as an introduction to the responsibilities and details of the role. At this orientation you also get a chance to connect with the other volunteers for the season.</li>
+    <li>In order to answer questions about Loving Spoonful clearly and with ease, training will be provided where you use your knowledge to develop a “pitch” about Loving Spoonful.</li>
+    <li>This training occurs in April, May, and June, or as needed with new volunteers who join mid-season.
+    </ul>
+    <p>You are always welcome and encouraged to contact us to share your successes, discuss any concerns or provide feedback: </p>
+    <ul>
+    <li>Alex Harper, Food Access Animator: 343-333-0244 (call or text), <a href="mailto:Alex@lovingspoonful.org">Alex@lovingspoonful.org</a></li>
+    <li>Mary Gauthier, Volunteer & Outreach Coordinator: <a href="mailto:Mary@lovingspoonful.org">Mary@lovingspoonful.org</a></li>
+    </ul>
+    <h3>Benefits &ndash; What you&rsquo;ll gain</h3>
+    <ul>
+    <li>Great experience teaching and working with youth</li>
+    <li>The chance to work in – and learn more about – vegetable gardens</li>
+    <li>Cost of CPIC will be reimbursed with submission of a receipt</li>
+    <li>Reference letter supplied for successful volunteer experience</li>
     </ul>
 </div>
 @endsection
