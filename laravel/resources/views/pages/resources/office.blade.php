b3f54f6be28f96fe4f17572b0cab92da1bd058b8
Loving Spoonful <41695559+lovingspoonful@users.noreply.github.com>
2018-08-24 19:48:28 -0400

    Content update

--- laravel/resources/views/pages/resources/office.blade.php
+++ laravel/resources/views/pages/resources/office.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="col-sm-2"></div>
 <div class="col-sm-8">
-    <h1>Position Title: Office Assistant</h1>
+    <h1>Administrative Helpers</h1>
     <h3>Position Purpose &ndash; Your impact</h3>
-    <p>Office Assistants give of their time and skills towards helping to create a strong administrative foundation at Loving Spoonful, which is key to providing consistent quality programming. In this way, their contributions directly support Loving Spoonful&rsquo;s goals of enhancing access to healthy food for all; engaging diverse sectors of the community; and delivering innovative and impactful programming.</p>
+    <p>The skills and expertise of Administrative Helpers assist in creating a strong administrative foundation at Loving Spoonful, which is key to providing consistent quality programming. In this way, their contributions directly support Loving Spoonful’s goals of enhancing access to healthy food for all; engaging diverse sectors of the community; and delivering innovative and impactful programming.</p>    <p>The GROW Project is one of several programs that directly support Loving Spoonful’s goals of enhancing access to healthy food for all; engaging diverse sectors of the community; and delivering innovative and impactful programming.
     <h3>Qualifications/Resources &ndash; What you&rsquo;ll need</h3>
     <ul>
     <li>Familiarity with Microsoft Excel, Microsoft Word, and computers is preferred.</li>
@@ -13,20 +13,24 @@
     <li>A flexible schedule is an asset.</li>
     </ul>
     <h3>Key Duties and Responsibilities</h3>
-    <p>Tasks of Office Assistants vary according to need. Some examples of what they do include, but not limited to:</p>
+    <p>Tasks of Administrative Helpers vary according to need. Some examples of what they do include, but not limited to:</p>
     <ul>
-    <li>Data entry &amp; database management</li>
+    <li>Data entry & database management </li>
     <li>Graphic design</li>
     <li>Building resources</li>
-    <li>Organizing hard copy and electronic files</li>
+    <li>Organizing hard copy and electronic files </li>
     <li>Digitizing historic archives</li>
     <li>Website development</li>
+    </ul>
+    <p><u>Other responsibilities</u></p>
+    <ul>
+    <li>Sign up for shifts on the online Volunteer Robot</li>
+    <li>Cancel shifts on the online Volunteer Robot at least 3 days in advance if going to be absent, or directly inform the Loving Spoonful staff as soon as possible if last minute changes occur.</li>
     </ul>
     <h3>Time Commitments and Locations</h3>
     <ul>
     <li>Shifts take place at the office of Loving Spoonful</li>
     <li>Ideally volunteers would be able to dedicate 2 office hours per week; however, as much time that can be given to tasks will be accommodated.</li>
-    </ul>
     <h3>Support &ndash; What we&rsquo;ll provide</h3>
     <ul>
     <li>Training will take place as needed.</li>
@@ -34,25 +38,16 @@
     <li>Flexibility in accommodating your schedule</li>
     <li>An online account with Volunteer Robot to track your shifts</li>
     </ul>
+    <p>You are always welcome and encouraged to contact us to share your successes, discuss any concerns or provide feedback: </p>
+    <ul>
+    <li>Melanie Redman, Office Manager: <a href="mailto:Mel@lovingspoonful.org">Mel@lovingspoonful.org</a></li>
+    <li>Mary Gauthier, Volunteer & Outreach Coordinator: <a href="mailto:Mary@lovingspoonful.org">Mary@lovingspoonful.org</a></li>
+    </ul>
     <h3>Benefits &ndash; What you&rsquo;ll gain</h3>
     <ul>
     <li>An opportunity to use your skills to support a worthy cause</li>
     <li>An opportunity to gain administrative experience</li>
-    <li>A reference letter supplied for successful volunteer experience</li>
-    </ul>
-    <h3>Measures of Success</h3>
-    <ul>
-    <li>Tasks are completed as assigned</li>
-    <li>Administrative resources that volunteers create/maintain create efficiency and/or provide useful tools in the office and/or for programs.</li>
-    </ul>
-    <h3>Responsible to:</h3>
-    <p>Melanie Redman, Office Manager<br /> Cell: 613-770-6105<br /><a href="mailto:mel@lovingspoonful.org">mel@lovingspoonful.org</a></p>
-    <h3>Interested? &ndash; Your next steps</h3>
-    <ul>
-    <li>Submit a Volunteer Application Form</li>
-    <li>Review and sign off on the Loving Spoonful Volunteer Rights &amp; Code of Conduct</li>
-    <li>Receive an account with Volunteer Robot (provided)</li>
-    <li>Attend a Loving Spoonful Orientation</li>
+    <li>Reference letter supplied for successful volunteer experience</li>
     </ul>
 </div>
 @endsection
