44f67674bf72262b8c052dcc7952a96b2a6c1e88
Loving Spoonful <41695559+lovingspoonful@users.noreply.github.com>
2018-08-25 00:08:13 -0400

    minor edits

--- laravel/resources/views/pages/resources/circles.blade.php
+++ laravel/resources/views/pages/resources/circles.blade.php
@@ -8,7 +8,7 @@
     <p>Circles&reg; is a proven poverty reduction program, and is offered by Loving Spoonful, in partnership with Kingston Community Health Centres (KCHC) and the City of Kingston. The weekly gatherings around meals build community across class lines, teach food skills, and support people experiencing poverty toward their goals for self-sufficiency.</p>
     <p>Circles&reg; is one of several programs that directly support Loving Spoonful&rsquo;s goals of enhancing access to healthy food for all; engaging diverse sectors of the community; and delivering innovative and impactful programming.</p>
     <h3>Position Purpose &ndash; Your impact</h3>
-    <p>Creating a space where participants can build food literacy, skills, confidence and community around good food, Circles® Program Assistants are critical in helping to ensure that the cooking classes run smoothly and are positive experiences for vulnerable adults and families. Workshop Facilitators will observe the positive effects of their presence week-to-week as participants’ skills improve, confidence builds, and friendships begin.</p>
+    <p>Creating a space where participants can build food literacy, skills, confidence and community around good food, Circles® Program Assistants are critical in helping to ensure that the cooking classes run smoothly and are positive experiences for vulnerable adults and families. Workshop Assistants will observe the positive effects of their presence week-to-week as participants’ skills improve, confidence builds, and friendships begin.</p>
     <h3>Qualifications/Resources &ndash; What you&rsquo;ll need</h3>
     <ul>
     <li>A non-judgemental and respectful approach is always required when working with cooking participants, ensuring participants feel welcome, included, and dignified.</li>
@@ -19,7 +19,7 @@
     <li>A Criminal Record Check is required, as program participants are allowed to bring their children to the classes. (Cost is reimbursed.)</li>
     </ul>
     <h3>Key Duties and Responsibilities</h3>
-    <p>Circles&reg; Program Assistants have on-site duties and other responsibilities.</p>
+    <p>Circles&reg; Workshop Assistants have on-site duties and other responsibilities.</p>
     <p><u>On-site Duties</u></p>
     <ul>
     <li>Assist in leading a small group of participants through the workshop recipes; allowing participants to take the lead on making the meal and demonstrating specific skills/tips as required.</li>
@@ -39,7 +39,7 @@
     <h3>Support &ndash; What we&rsquo;ll provide</h3>
     <ul>
     <li>A training meeting will be set up as at the start of each cooking program to review details about the cooking participants and volunteer position, as well as the recipes and activities that will be used during the workshops. This meeting will either take place before the program begins or directly before the first shift at the kitchen.</li>
-    <li>The Kitchen Instructor will provide on-going support. Supervision will take place to make sure recipes are being followed (within reason) and people are having fun.</li>
+    <li>Loving Spoonful staff will provide on-going support. Supervision will take place to make sure recipes are being followed (within reason) and people are having fun.</li>
     <li>An account with our online Volunteer Robot to view and sign up for shifts.</li>
     <li>An orientation session to learn more about Loving Spoonful, the Circles&reg; Program, and Volunteer Robot.</li>
     </ul>
