cf0b685c05a76c25994e239c91e62c6d014593f8
Loving Spoonful <41695559+lovingspoonful@users.noreply.github.com>
2018-08-24 22:19:28 -0400

    Content update to standardized format

--- laravel/resources/views/pages/resources/gleaning.blade.php
+++ laravel/resources/views/pages/resources/gleaning.blade.php
@@ -3,9 +3,51 @@
 @section('content')
 <div class="col-sm-2"></div>
 <div class="col-sm-8">
-    <h2>Gleaning</h2>
-    <p>Gleaning volunteers collect leftover crops from farmers' fields after they have been commercially harvested or on fields where it is not economically profitable to harvest. This food is then either processed for or delivered to 40+ local shelters, meal programs, and social service agencies in Kingston.</p>
-    <p>Volunteers drive or carpool to a local farm or garden to harvest produce. Produce is weighed at the end of the shift, delivered to Loving Spoonful&rsquo;s Little Cat Cold Storage, and the delivery details are recorded on the track sheet there.</p>
-    <p>Gleaning occurs in the harvest season, August to November. A one-time commitment is all that is required. Equipment is provided. No training or experience necessary, but this will involve light physical labour and occasional heavy lifting.</p>
+    <h1>Gleaners</h1>
+    <h3>Program Description</h3>
+    <p>Working with local farmers, youth, and community volunteers, the Gleaning Project harvests surplus crops and delivers the produce to 40+ shelters, meal programs, and Fresh Food Market Stands.</p> 
+    <p>Gleaning is one of several programs that directly support Loving Spoonful’s goals of enhancing access to healthy food for all; engaging diverse sectors of the community; and delivering innovative and impactful programming.</p>
+    <h3>Position Purpose &ndash; Your impact</h3>
+    <p>Gleaning volunteers have the key role of collecting surplus crops from farmers' fields after they have been commercially harvested or on fields where it is not economically profitable to harvest. Gleaners reduce food waste and directly help redistribute food to Kingston communities who need it.</p>
+    <h3>Qualifications/Resources &ndash; What you&rsquo;ll need</h3>
+    <ul>
+    <li>This position requires light physical labour and occasional heavy lifting.</li>
+    <li>No training or previous experience required.</li>
+    </ul>
+    <h3>Key Duties and Responsibilities</h3>
+    <ul>
+    <li>Volunteers drive or carpool to a local farm or garden to harvest produce.</li>
+    <li>Produce is weighed at the end of the shift.</li>
+    <li>Produce is delivered to Loving Spoonful’s Little Cat Cold Storage.</li>
+    <li>Record delivery details on the track sheet there.</li>
+    </ul>
+    <p><u>Other responsibilities</u></p>
+    <ul>
+    <li>Sign up for shifts on the online Volunteer Robot</li>
+    <li>Cancel shifts on the online Volunteer Robot at least 3 days in advance if going to be absent, or directly inform the Loving Spoonful staff as soon as possible if last minute changes occur.</li>
+    </ul>
+    <h3>Time Commitments and Locations</h3>
+    <ul>
+    <li>Gleaning occurs in the harvest season, August to November.</li>
+    <li>Volunteer shifts are 2 to 3 hours.</li>
+    <li>A one-time commitment is all that is required.</li>
+    <li>Gleaning opportunities are posted on the Volunteer Robot.</li>
+    </ul>
+    <h3>Support &ndash; What we&rsquo;ll provide</h3>
+    <ul>
+    <li>Any equipment, if needed, is provided. </li>
+    <li>A Loving Spoonful staff will always be present to provide guidance and support.</li>
+    </ul>
+    <p>You are always welcome and encouraged to contact us to share your successes, discuss any concerns or provide feedback: </p>
+    <ul>
+    <li>Bree Rappaport, Gleaning Coordinator: <a href="mailto:Gleaning@lovingspoonful.org">Gleaning@lovingspoonful.org</a></li>
+    <li>Mary Gauthier, Volunteer & Outreach Coordinator: <a href="mailto:Mary@lovingspoonful.org">Mary@lovingspoonful.org</a></li>
+    </ul>
+    <h3>Benefits &ndash; What you&rsquo;ll gain</h3>
+    <ul>
+    <li>Time outdoors, on farms or in gardens</li>
+    <li>Seeing beautiful produce be salvaged</li>
+    <li>Reference letter supplied for successful volunteer experience</li>
+    </ul>
 </div>
 @endsection
