aa691dc0173ee87368f214e06ae2cbcfb02dac5d
Kahvi <iamkahvi@gmail.com>
2018-08-26 16:32:25 -0400

    removed hide empty days feature for retesting

--- laravel/resources/views/pages/event/view.blade.php
+++ laravel/resources/views/pages/event/view.blade.php
@@ -128,7 +128,6 @@
                     </select>
                 </div>
 
-
             </form>
 
             <hr>
@@ -136,7 +135,7 @@
             <h2>All Shifts:</h2>
 
             <div class="days">
-                @foreach($event->days(true) as $day)
+                @foreach($event->days() as $day)
                     <div class="day" data-date="{{ $day->date->format('Y-m-d') }}">
                         <div class="heading">
                             <h3><ins>{{ date("D, M jS", strtotime($day->date->format('Y-m-d'))) }}</ins></h3>
