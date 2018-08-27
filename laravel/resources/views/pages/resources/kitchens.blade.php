c2161306a0a96e7449f1638dbd59359b6216bebd
Loving Spoonful <41695559+lovingspoonful@users.noreply.github.com>
2018-08-26 23:29:54 -0400

    Measures of Success Reinstated, title change

--- laravel/resources/views/pages/resources/kitchens.blade.php
+++ laravel/resources/views/pages/resources/kitchens.blade.php
@@ -3,15 +3,16 @@
 @section('content')
 <div class="col-sm-2"></div>
 <div class="col-sm-8">
-    <h1>Community Kitchens Workshop Facilitator</h1>
+    <h1>Community Kitchens Workshop Assistant</h1>
     <h3>Program Description</h3>
     <p>Loving Spoonful’s Community Kitchens are designed to get people together around good food. We cook with people of all ages, backgrounds, and abilities – children, youth, adults, families, newcomers, Indigenous community members, people with physical and intellectual differences, expert home cooks, and beginners. We get together to make healthy, affordable, and simple recipes in kitchens throughout Kingston. Hands-on learning and discussions of food topics helps build knowledge, skills, and confidence, encouraging participants to make healthy and informed food choices in the kitchen and beyond. In addition to food literacy development, our community kitchens are places where people socialize, building community connections and friendships around food. They also provide good food access as surplus food, healthy recipes, spices, and cookware are sent home to assist with cooking after programs are complete.</p>
     <p>Community Kitchens is one of several programs that directly support Loving Spoonful’s goals of enhancing access to healthy food for all; engaging diverse sectors of the community; and delivering innovative and impactful programming.
     <h3>Position Purpose &ndash; Your impact</h3>
-    <p>Creating a space where participants can build food literacy, skills, confidence, and community around good food, Community Kitchen Workshop Facilitators are critical in helping to ensure that the cooking classes run smoothly and are positive experiences for all participants. They provide extra hands, eyes and words of support and encouragement. Workshop Facilitators will observe the positive effects of their presence week-to-week as participants’ skills improve, confidence builds, and friendships begin.</p>
+    <p>Creating a space where participants can build food literacy, skills, confidence, and community around good food, Community Kitchen Workshop Assistants are critical in helping to ensure that the cooking classes run smoothly and are positive experiences for all participants.</p>
     <h3>Qualifications/Resources &ndash; What you&rsquo;ll need</h3>
     <ul>
-    <li>A non-judgemental and respectful approach is always required when working with cooking participants, ensuring participants feel welcome, included, and dignified</li>   <li>Knowledge of basic gardening techniques such as “planting”, “weeding”, “thinning” and “harvesting” would be helpful for spring workshops, but is not required.</li>
+    <li>A non-judgemental and respectful approach is always required when working with cooking participants, ensuring participants feel welcome, included, and dignified</li>   
+    <li>Knowledge of basic gardening techniques such as “planting”, “weeding”, “thinning” and “harvesting” would be helpful for spring workshops, but is not required.</li>
     <li>Some cooking skills/knowledge</li>
     <li>An outgoing attitude and an ability to engage with others</li>
     <li>Be comfortable standing for long periods of time (1.5 – 3 hrs depending on the program)</li>
@@ -43,7 +44,7 @@
     <ul>
     <li>A training meeting will be set up as at the start of each cooking program to review details about the cooking participants and volunteer position, as well as the recipes and activities that will be used during the workshops. This meeting will either take place before the program begins or directly before the first shift at the kitchen.</li>
     <li>The Community Kitchen Instructor will provide on-going support. Supervision will take place to make sure recipes are being followed (within reason) and people are having fun. </li>
-    <li>Informal post-class debriefings with the Community Kitchen Instructor allows Workshop Facilitators to discuss what is going well with the program, areas for improvement, as well as voice any suggestions, comments or concerns.</li>
+    <li>Informal post-class debriefings with the Community Kitchen Instructor allows Workshop Assistants to discuss what is going well with the program, areas for improvement, as well as voice any suggestions, comments or concerns.</li>
     <li>An account with Loving Spoonful’s Volunteer Robot to view and sign up for shifts</li>
     </ul>
     <p>You are always welcome and encouraged to contact us to share your successes, discuss any concerns or provide feedback: </p>
@@ -59,5 +60,7 @@
     <li>Cost of CPIC will be reimbursed with submission of a receipt</li>
     <li>Reference letter supplied for successful volunteer experience, upon request.</li>
     </ul>
+    <h3>Measures of Success</h3>
+    <p>Workshop Assistants will observe the positive effects of their presence week-to-week as participants’ skills improve, confidence builds, and friendships begin.</p>
 </div>
 @endsection
