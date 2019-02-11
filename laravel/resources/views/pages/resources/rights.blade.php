e991bc26006c7e3d0cfed600d4eb868df0ff8efa
Loving Spoonful <41695559+lovingspoonful@users.noreply.github.com>
2019-02-11 11:01:05 -0500

    CPIC policy updated

--- laravel/resources/views/pages/resources/rights.blade.php
+++ laravel/resources/views/pages/resources/rights.blade.php
@@ -41,6 +41,16 @@
     <li>participating in any decisions that could result in personal benefit.</li>
     </ul>
     <p>It is important to let your supervisor know about potential conflicts of interests so that they can be resolved or avoided.</p>
+    <h3>Criminal Record Check (CPIC)</h3>
+    <p>Volunteering in the GROW Project and Community Kitchens often involves working with vulnerable populations including children, people with intellectual disabilities and people experiencing mental illness.  For that reason, a current criminal records check (CPIC) with a Vulnerable Sector check is required for all new volunteers participating in these programs. The document must be current within 2 years of the date of your volunteer application. CPICs are not required to volunteer with any other Loving Spoonful program, unless otherwise indicated.</p>
+    <p>Your CPIC can be obtained by visiting the local police department. Loving Spoonful does not keep the original CPIC on file, but a Loving Spoonful staff person must see it. A scanned copy or photo of the document is acceptable. So long as you are an active volunteer in GROW Project and Community Kitchens, a yearly declaration will be requested to confirm that the status of the CPIC has not changed.</p>
+    <p>If you are able to cover the cost of your CPIC, that is appreciated as it allows our budget to provide more programming.  If you are able to do so, Loving Spoonful can provide a tax receipt for your donation if you request one. If you would like Loving Spoonful to reimburse the cost of the CPIC ($30), please submit the receipt from your payment.  Loving Spoonful will reimburse your expense after you have completed five (5) volunteer shifts in one of these programs.</p>
+    <h3>Use of drugs and alcohol</h3>
+    <p>Volunteers must not attend their volunteer shift while under the influence of drink or drugs. To do so will be cause for instant dismissal. Volunteers must not take drugs or drink during their shift at Loving Spoonful.</p>
+    <h3>Call staff for support when needed</h3>
+    <p>Volunteers must call a staff person if there is an incident involving community members or in case of an emergency. Do not deal with incidents on your own.</p>
+    <h3>Attendance</h3>
+    <p>In order to provide our services to the community, we need volunteers who are reliable in turning up for their shifts. Please arrive for your shift on time. Please call your supervisor let us know in advance if you are not able to attend so that we can try to fill your place.</p>
     <h3>Volunteer Rights</h3>
     <p>As a volunteer, Loving Spoonful staff members and supervisors are here to provide you with support. We strongly encourage you to contact staff if you feel that any of the following rights are not being met while you are in your role as a volunteer.</p>
     <p>You have the right:</p>
@@ -50,11 +60,5 @@
     <li>To be treated with respect. If you feel that you have not been treated with respect, please talk to your supervisor or address it with another staff member with whom you feel comfortable.</li>
     <li>To have your confidential and personal contact information protected by Loving Spoonful in accordance with the Personal Information Protection and Electronic Documents Act (2000).</li>
     </ul>
-    <h3>Use of drugs and alcohol</h3>
-    <p>Volunteers must not attend their volunteer shift while under the influence of drink or drugs. To do so will be cause for instant dismissal. Volunteers must not take drugs or drink during their shift at Loving Spoonful.</p>
-    <h3>Call staff for support when needed</h3>
-    <p>Volunteers must call a staff person if there is an incident involving community members or in case of an emergency. Do not deal with incidents on your own.</p>
-    <h3>Attendance</h3>
-    <p>In order to provide our services to the community, we need volunteers who are reliable in turning up for their shifts. Please arrive for your shift on time. Please call your supervisor let us know in advance if you are not able to attend so that we can try to fill your place.</p>
 </div>
 @endsection
