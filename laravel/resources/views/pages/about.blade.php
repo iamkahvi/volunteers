5102728f92300871ec5fd04109bdd607f9f96a69
Loving Spoonful <41695559+lovingspoonful@users.noreply.github.com>
2019-10-21 13:12:09 -0400

    Changes to content

--- laravel/resources/views/pages/about.blade.php
+++ laravel/resources/views/pages/about.blade.php
@@ -6,17 +6,24 @@
     <h1>About the Volunteer Database</h1>
 
     <h4>
-        This website is a scheduling system where users can sign up for volunteer shifts.
+        This website is a scheduling system where users can sign up for volunteer shifts. It is also where volunteers can find out more about all current volunteer opportunities with Loving Spoonful, including program descriptions, volunteer role descriptions and Program Coordinator contact details. The Volunteer Handbook and other volunteer resources are also found on Love Bug.
     </h4>
-
+    
     <hr>
+    
+    <h2>Dashboard</h2>
+    <p>
+       The Dashboard shows all the current volunteer opportunities. Click on the opportunity to find a description of the program, a description of the volunteer role, and a calendar of available shifts that you can sign up for directly. You will also find the contact information of the Program Coordinator here.
+    </p>
 
-    <h2>Navigation Bar:</h2>
+    <h2>Navigation Bar</h2>
     <br>
     <ul>
-    <li><strong>Resources</strong>: These pages are where you will find reading material to educate you on programming and the Love Bug!</li>
+    <li><strong>Resources</strong>: These pages are where you will find information about how to use Love Bug, as well as more detailed information about our programs and the volunteer roles. Any volunteer resources for particular opportunities will also be found in this section.</li>
     </ul>
+    <p style="padding-left: 60px;"><strong>Love Bug Instructions</strong></p>
     <p style="padding-left: 60px;"><strong>Volunteer Handbook</strong></p>
+    <p style="padding-left: 60px;"><strong>Volunteer Code of Conduct</strong></p>
     <p style="padding-left: 60px;"><strong>Fresh Food Delivery</strong></p>
     <p style="padding-left: 60px;"><strong>GAR Market Booths</strong></p>
     <p style="padding-left: 60px;"><strong>Community Kitchens</strong></p>
@@ -24,7 +31,7 @@
     <p style="padding-left: 60px;"><strong>Circles</strong></p>
     <p style="padding-left: 60px;"><strong>Gleaning</strong></p>
     <p style="padding-left: 60px;"><strong>Events</strong></p>
-    <p style="padding-left: 60px;"><strong>Instructions</strong></p>
+    <p style="padding-left: 60px;"><strong>Office Administration</strong></p>
     <ul>
     <li><strong>Your Shifts</strong>: This page is where you will find all the shifts you have signed up for; past, present and future.</li>
     </ul>
@@ -32,16 +39,16 @@
     <li><strong>Programs</strong>: This page is where you will find all upcoming, ongoing and past programs.</li>
     </ul>
     <ul>
-    <li><strong>[User]</strong>: This page is where will find all your personal details.</li>
+    <li><strong>User</strong>: This page is where you will find all your personal details.</li>
     </ul>
     <ul>
-    <li><strong>Logout</strong>: Clicking this button logs you out of the Robot.</li>
+    <li><strong>Logout</strong>: Clicking this button logs you out of Love Bug.</li>
     </ul>
     <br>
     <h2>Viewing a Program</h2>
     <br>
-    <p><strong>Description</strong>: This is a description of the program in question.</p>
-    <p><strong>Legend</strong>: This is a colour-coded legend to let you know what shift are available, taken or yours.</p>
+    <p><strong>Description</strong>: This is a description of the program.</p>
+    <p><strong>Legend</strong>: This is a colour-coded legend to let you know what shifts are available, taken or yours.</p>
     <p><strong>Filters</strong>: You have the option to filter by Activity, Week and Day.</p>
     <p><strong>All Shifts</strong>:</p>
     <ul>
@@ -55,7 +62,7 @@
     <ol>
     <li>From the &ldquo;Programs&rdquo; page, click on a program</li>
     <li><em>Use the </em><strong>Filters</strong> to specify the date-range you are able to volunteer</li>
-    <li><em>Use the </em><strong>Filters</strong> to specify the Activity you are able to volunteer for</li>
+    <li><em>Use the </em><strong>Filters</strong> to specify the Activity you interested in volunteering for</li>
     <li>Scroll through the days listed</li>
     <li>Click on a green bubble to view a shift</li>
     <ul>
@@ -71,63 +78,5 @@
     <li>Click Release Shift (You must perform this action<strong> 3 days</strong> in advance of the shift occurrence)</li>
     </ol>
     <p><br /><br /></p>
-
-    <hr>
-
-    <h2><ins>Features</ins></h2>
-
-    <h3>Mobile friendly</h3>
-
-    <p>
-        This website was developed using the mobile first methodology; all parts of the site are designed to work on phones, tablets, and desktop computers.
-    </p>
-
-    <h3>User roles</h3>
-
-    <p>
-        Every user has specific roles that allow them to take certain shifts.
-        This allows administrators to create medical or fire fighting shifts that only allow approved users.
-    </p>
-
-    <h3>User uploads</h3>
-
-    <p>
-        Users can upload files into their profile to be approved by administrators.
-        This allows users to upload their EMT or fire fighting certification documents.
-    </p>
-
-    <h3>History from previous events</h3>
-
-    <p>
-        This website supports creating multiple events, allowing use by many festivals, or allowing a festival to keep a record of who volunteered in the past.
-    </p>
-
-    <h3>Customizable shifts</h3>
-
-    <p>
-        Event shifts can start and end at any time of day and can be any duration.
-        Instead of being locked into a rigid table, this lets administrators create longer shifts for leads and shorter shifts for miscellaneous jobs.
-    </p>
-
-
-    <hr>
-
-    <h2><ins>History</ins></h2>
-
-    <p>
-        Back between late 2014 and early 2015, volunteers from Apogaea were working on making updates to their existing volunteer database.
-        <a href="https://github.com/itsrachelfish">Rachel Fish</a> is friends with a couple of the developers and was present at a weekend-long hackathon where lots of ideas were shared and code was written aplenty.
-        Unfortunately, when Apogaea was postponed in 2015, development of their volunteer database was postponed as well.
-    </p>
-
-    <p>
-        This project was started in November of 2015 as an experiment in learning the <a href="http://laravel.com/">Laravel framework</a>.
-        Over time, what started as a learning exercise turned into a fully featured system that might actually be useful to someone.
-        Rachel's previous experience with the old Apogaea database gave her insight into some of the problems the team was facing and inspired her to try something new.
-    </p>
-
-    <p>
-        Writing in the third person is weird.
-    </p>
 </div>
 @endsection
