75bb6490ab0bd06a305ed405d797dd0139cec7c5
lovingspoonful2 <52539273+lovingspoonful2@users.noreply.github.com>
2019-11-05 13:35:13 -0500

    Update content, add resources

--- laravel/resources/views/pages/resources/grow.blade.php
+++ laravel/resources/views/pages/resources/grow.blade.php
@@ -5,13 +5,13 @@
 <div class="col-sm-8">
     <h1>GROW Project Workshop Assistant</h1>
     <h3>Program Description</h3>
-    <p>Loving Spoonful's school-garden based GROW Project teaches over 600 students at 17 Kingston and area schools about good food, community, and collaboration. In each school, students in grades 4 to 8 participate in seven curriculum-connected classroom workshops that complement their garden time, teaching students about food systems, social justice and food security, food and culture, and healthy gardens and cooking. Visits from local farmers and nature-based field trips further assist students to make connections between their food, their health, their community, and the Earth.</p>
+    <p>Loving Spoonful's school-garden based GROW Project teaches over 800 students at 21 Kingston and area schools about good food, community, and collaboration. In each school, students in grades 4 to 8 participate in seven curriculum-connected classroom workshops that complement their garden time, teaching students about food systems, social justice and food security, food and culture, and healthy gardens and cooking. Visits from local farmers and nature-based field trips further assist students to make connections between their food, their health, their community, and the Earth.</p>
     <p>The GROW Project is one of several programs that directly support Loving Spoonful’s goals of enhancing access to healthy food for all; engaging diverse sectors of the community; and delivering innovative and impactful programming.
     <h3>Position Purpose &ndash; Your impact</h3>
     <p>GROW Project Workshop Assistants help out in hands-on workshops where students design, plant, care for and harvest vegetables in their school gardens while learning values of stewardship and responsibility.</p>
     <h3>Qualifications/Resources &ndash; What you&rsquo;ll need</h3>
     <ul>
-    <li>A Criminal Record Check with ‘no criminal record’ is required. Cost is reimbursed; just provide your receipt.</li>
+    <li>A Criminal Record/Vulnerable Sector Check with ‘no criminal record’ is required. Cost ($35) can be reimbursed, if not donated, after completing 5 volunteer shifts. Just provide your receipt.</li>
     <li>Ability to lead small groups (e.g. chopping vegetables, sowing seeds, looking at vermi-composting worms); a staff person from Loving Spoonful will always be present.</li>
     <li>Knowledge of basic gardening techniques such as “planting”, “weeding”, “thinning” and “harvesting” would be helpful for spring workshops, but is not required.</li>
     <li>Knowledge of basic food preparation techniques (chopping, mixing, using different ingredients) would be helpful but is not required.</li>
@@ -34,8 +34,9 @@
     <li>Cancel shifts on the online Love Bug at least 3 days in advance if going to be absent, or directly inform the Loving Spoonful staff as soon as possible if last minute changes occur.</li>
     </ul>
     <h3>Time Commitments and Locations</h3>
-    <p>Over the school year, the GROW Project delivers 7 workshops to each of 17 different schools in the City of Kingston. GROW Workshop Assistants are needed for 5 of these workshops (85 workshops in total) which take place in: late Sept/early Oct; November; April; May; and June.</p>
+    <p>Over the school year, the GROW Project delivers 7 workshops to each of 21 different schools in the City of Kingston. GROW Workshop Assistants are needed for 5 of these workshops which take place in 2-3 week sessions in late Sept/early Oct; November; April; May; and June.</p>
     <p>All scheduled GROW Workshops are posted on Loving Spoonful’s online Love Bug, and each GROW Workshop Assistant will be provided an online account to sign up to volunteer for any workshop. Workshop shifts can occur any week day, Monday to Friday, during school hours, and are approximately 2-3 hours long.</p>
+    <p>Although volunteers are not obligated to commit to a specific number of shifts, with 140 volunteer shifts needing to be filled during each round of GROW Project workshops, volunteers are encouraged to sign up for as many shifts as possible.</p>
     <h3>Support &ndash; What we&rsquo;ll provide</h3>
     <ul>
     <li>Hands-on, on-site training by GROW Project Staff</li>
@@ -56,6 +57,12 @@
     <li>Show an increased awareness of food systems, food security and the importance of food in culture</li>
     <li>Build a connection to the environment and working together to support the local community</li>
     <li>Develop life skills such as gardening, healthy food preparation, and critical thinking</li>
+    </ul>
+    <h3>Workshop Resources for Volunteers</h3>
+    <p><u>Workshop 3</u></p>
+    <ul>
+    <li><a href="https://www.dropbox.com/s/l41gg01zqqmyz1a/GROW%20Project.F3_Kale%26Culture.Gr4-6_Volunteer%20Overview.pdf?dl=0">"Kale & Culture" (Gr.4-6) - Volunteer Overview</a></li>
+    <li><a href="https://www.dropbox.com/s/u8kp422lp6wu46s/GROW%20Workshop%203%20Activity_Corn%20%26%20Culture.pdf?dl=0">"Corn & Culture" (Gr.7-8) - Volunteer Overview</a></li>
     </ul>
     </div>
 @endsection
